76c74b24dfb9b92165ca2bce5e5a807be8156006
YamiTeyssier <97235275+YamiTeyssier@users.noreply.github.com>
2023-05-09 08:58:07 -0300

    test push github

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -52,7 +52,7 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          title: const Text('Login Plugin example app'),
         ),
         body: Center(
           child: Text('Running on: $_platformVersion\n'),
