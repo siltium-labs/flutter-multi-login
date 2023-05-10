1d26294108e8e60e9f09ee5ed8e24d063d9e5b90
YamiTeyssier <97235275+YamiTeyssier@users.noreply.github.com>
2023-05-10 12:20:53 -0300

    Add apple sing in (ios) & ajustes

--- lib/src/manager/auth_manager.dart
+++ lib/src/manager/auth_manager.dart
@@ -1,7 +1,13 @@
+//* Dart imports
+import 'dart:convert';
+import 'dart:math';
+
 //* Package imports
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:sign_in_with_apple/sign_in_with_apple.dart';
+import 'package:crypto/crypto.dart';
 
 //* Project imports
 import 'package:s_multiloginp/src/models/current_user_model.dart';
@@ -44,13 +50,13 @@ class AuthManager {
           await googleUser?.authentication;
 
       // Create a new credential (guardar credenciales)
-      final credential = GoogleAuthProvider.credential(
+      final googleOAuthCredential = GoogleAuthProvider.credential(
         accessToken: googleAuth?.accessToken,
         idToken: googleAuth?.idToken,
       );
 
       // Once signed in, return the UserCredential (inicio de sesion)
-      user = await _auth.signInWithCredential(credential);
+      user = await _auth.signInWithCredential(googleOAuthCredential);
       return user;
     } on FirebaseAuthException catch (e) {
       await onFirebaseAuthException(error: e);
@@ -67,11 +73,11 @@ class AuthManager {
       );
 
       // Create a credential from the access token (guardar credenciales)
-      final OAuthCredential facebookAuthCredential =
+      final OAuthCredential facebookOAuthCredential =
           FacebookAuthProvider.credential(loginResult.accessToken!.token);
 
       // Once signed in, return the UserCredential (inicio de sesion)
-      user = await _auth.signInWithCredential(facebookAuthCredential);
+      user = await _auth.signInWithCredential(facebookOAuthCredential);
       return user;
     } on FirebaseAuthException catch (e) {
       await onFirebaseAuthException(error: e);
@@ -79,15 +85,49 @@ class AuthManager {
     }
   }
 
-  //TODO: APPLE
+  //TODO: APPLE (para ios)
+  // Metodos necesarios para el Apple SingIn
+  String generateNonce([int length = 32]) {
+    const charset =
+        '0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._';
+    final random = Random.secure();
+    return List.generate(length, (_) => charset[random.nextInt(charset.length)])
+        .join();
+  }
+
+  String sha256ofString(String input) {
+    final bytes = utf8.encode(input);
+    final digest = sha256.convert(bytes);
+    return digest.toString();
+  }
+
+  // Apple SingIn
   Future<UserCredential?> signInWithApple() async {
-    // final appleProvider = AppleAuthProvider();
-    // if (kIsWeb) {
-    //   user = await _auth.signInWithPopup(appleProvider);
-    // } else {
-    //   user = await _auth.signInWithProvider(appleProvider);
-    // }
-    return user;
+    try {
+      // Prevent replay attacks with the credential returned from Apple
+      final rawNonce = generateNonce();
+      final nonce = sha256ofString(rawNonce);
+
+      // Request credential for the currently signed in Apple account.
+      final appleCredential = await SignInWithApple.getAppleIDCredential(
+        scopes: [
+          AppleIDAuthorizationScopes.email,
+          AppleIDAuthorizationScopes.fullName,
+        ],
+        nonce: nonce,
+      );
+
+      // Create an `OAuthCredential` from the credential returned by Apple.
+      final appleOAuthCredential = OAuthProvider("apple.com").credential(
+        idToken: appleCredential.identityToken,
+        rawNonce: rawNonce,
+      );
+      user = await _auth.signInWithCredential(appleOAuthCredential);
+      return user;
+    } on FirebaseAuthException catch (e) {
+      await onFirebaseAuthException(error: e);
+      return null;
+    }
   }
 
   //OTROS
