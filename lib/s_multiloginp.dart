a4edd0821a14c9cce9640d09ca28a0c1ad4a7a3d
YamiTeyssier <97235275+YamiTeyssier@users.noreply.github.com>
2023-05-10 10:01:00 -0300

    complete MultiloginOptionsModel

--- lib/s_multiloginp.dart
+++ lib/s_multiloginp.dart
@@ -40,7 +40,17 @@ class SMultiLogin {
         appId: options.appId,
         messagingSenderId: options.messagingSenderId,
         projectId: options.projectId,
+        authDomain: options.authDomain,
+        databaseURL: options.databaseURL,
         storageBucket: options.storageBucket,
+        measurementId: options.measurementId,
+        // ios specific
+        trackingId: options.trackingId,
+        deepLinkURLScheme: options.deepLinkURLScheme,
+        androidClientId: options.androidClientId,
+        iosClientId: options.iosClientId,
+        iosBundleId: options.iosBundleId,
+        appGroupId: options.appGroupId,
       ),
     );
   }
@@ -59,14 +69,34 @@ class MultiloginOptionsModel {
   String appId;
   String messagingSenderId;
   String projectId;
-  String storageBucket;
+  String? authDomain;
+  String? databaseURL;
+  String? storageBucket;
+  String? measurementId;
+  // ios specific
+  String? trackingId;
+  String? deepLinkURLScheme;
+  String? androidClientId;
+  String? iosClientId;
+  String? iosBundleId;
+  String? appGroupId;
 
   MultiloginOptionsModel({
     required this.apiKey,
     required this.appId,
     required this.messagingSenderId,
     required this.projectId,
-    required this.storageBucket,
+    this.authDomain,
+    this.databaseURL,
+    this.storageBucket,
+    this.measurementId,
+    // ios specific
+    this.trackingId,
+    this.deepLinkURLScheme,
+    this.androidClientId,
+    this.iosClientId,
+    this.iosBundleId,
+    this.appGroupId,
   });
 }
 
