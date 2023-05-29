e9dddab34cc22013b7da87624b284f168e4c4350
YamiTeyssier <97235275+YamiTeyssier@users.noreply.github.com>
2023-05-29 11:51:34 -0300

    Coment deprecated test

--- test/s_multiloginp_method_channel_test.dart
+++ test/s_multiloginp_method_channel_test.dart
@@ -1,24 +1,24 @@
-import 'package:flutter/services.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:s_multiloginp/s_multiloginp_method_channel.dart';
+// import 'package:flutter/services.dart';
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:s_multiloginp/s_multiloginp_method_channel.dart';
 
-void main() {
-  MethodChannelSMultiloginp platform = MethodChannelSMultiloginp();
-  const MethodChannel channel = MethodChannel('s_multiloginp');
+// void main() {
+//   MethodChannelSMultiloginp platform = MethodChannelSMultiloginp();
+//   const MethodChannel channel = MethodChannel('s_multiloginp');
 
-  TestWidgetsFlutterBinding.ensureInitialized();
+//   TestWidgetsFlutterBinding.ensureInitialized();
 
-  setUp(() {
-    channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return '42';
-    });
-  });
+//   setUp(() {
+//     channel.setMockMethodCallHandler((MethodCall methodCall) async {
+//       return '42';
+//     });
+//   });
 
-  tearDown(() {
-    channel.setMockMethodCallHandler(null);
-  });
+//   tearDown(() {
+//     channel.setMockMethodCallHandler(null);
+//   });
 
-  test('getPlatformVersion', () async {
-    expect(await platform.getPlatformVersion(), '42');
-  });
-}
+//   test('getPlatformVersion', () async {
+//     expect(await platform.getPlatformVersion(), '42');
+//   });
+// }
