7cfee60bc8af2b845139a0a0257823cc65dffa5a
Nahuel <85951821+NahuelFedyszynSiltium@users.noreply.github.com>
2024-11-05 12:28:38 -0300

    test

--- lib/src/manager/facebook_login_manager.dart
+++ lib/src/manager/facebook_login_manager.dart
@@ -1,20 +1,67 @@
 //* Package imports
+import 'dart:convert';
+import 'dart:io';
+import 'dart:math';
+
+import 'package:crypto/crypto.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 
 class FacebookLoginManager {
+  String sha256ofString(String input) {
+    final bytes = utf8.encode(input);
+    final digest = sha256.convert(bytes);
+    return digest.toString();
+  }
+
+  String generateNonce([int length = 32]) {
+    const String charset =
+        '0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-.';
+    final random = Random.secure();
+    return List.generate(
+        length, (value) => charset[random.nextInt(charset.length)]).join();
+  }
+
   facebookLogin() async {
-    // Trigger the sign-in flow
-    final LoginResult loginResult = await FacebookAuth.instance.login(
-      loginBehavior: LoginBehavior.webOnly,
-    );
+    if (Platform.isIOS) {
+      final rawNonce = generateNonce();
+      final nonce = sha256ofString(rawNonce);
+      // Trigger the sign-in flow
+      final LoginResult loginResult = await FacebookAuth.instance.login(
+        // loginBehavior: LoginBehavior.webOnly,
+        loginTracking: LoginTracking.limited,
+        nonce: nonce,
+      );
 
-    // Create a credential from the access toke
-    final OAuthCredential facebookOAuthCredential =
-        FacebookAuthProvider.credential(loginResult.accessToken!.tokenString);
+      // Create a credential from the access toke
+      final OAuthCredential facebookOAuthCredential =
+          FacebookAuthProvider.credential(
+        loginResult.accessToken!.tokenString,
+      );
 
-    // Once signed in, return the UserCredential (inicio de sesion, en AuthManager)
-    return facebookOAuthCredential;
+      // Once signed in, return the UserCredential (inicio de sesion, en AuthManager)
+      // return facebookOAuthCredential;
+      final LimitedToken token =
+          facebookOAuthCredential.accessToken as LimitedToken;
+      return OAuthCredential(
+        providerId: 'facebook.com',
+        signInMethod: 'oauth',
+        idToken: token.tokenString,
+        rawNonce: rawNonce,
+      );
+    } else {
+      // Trigger the sign-in flow
+      final LoginResult loginResult = await FacebookAuth.instance.login(
+        loginBehavior: LoginBehavior.webOnly,
+      );
+
+      // Create a credential from the access toke
+      final OAuthCredential facebookOAuthCredential =
+          FacebookAuthProvider.credential(loginResult.accessToken!.tokenString);
+
+      // Once signed in, return the UserCredential (inicio de sesion, en AuthManager)
+      return facebookOAuthCredential;
+    }
   }
 
   facebookLogout() async {
